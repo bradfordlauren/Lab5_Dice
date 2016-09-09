49a4c7881bbe59399b1a7d6177c918b81f77c8d7
bradfordlauren <bradfordlauren@yahoo.com>
2016-09-09 12:48:01 -0400

    Lab 5 Complete w/ Extras

--- Lab5 Dice/Program.cs
+++ Lab5 Dice/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace Lab5_Dice
 {
-    class Program
+    public
+        class Program
     {
         static void Main(string[] args)
+
         {
+            Console.WriteLine("Welcome to the Grand Circus Casino!");
+            Random diceThrows = new Random();
+            string rollAgainAnswer;
+            //loop starts here
+            
+                Console.WriteLine("Roll the dice? (y/n)");
+                string rollDiceAnswer = Console.ReadLine();
+                int rollCounter = 1;
+
+            do
+            {
+                Console.WriteLine("Roll " + rollCounter + ":");
+                int diceNumber1 = diceThrows.Next(1, 7);
+                int diceNumber2 = diceThrows.Next(1, 7);
+                Console.WriteLine(diceNumber1);
+                Console.WriteLine(diceNumber2);
+
+                if(diceNumber1==1 && diceNumber2==1)
+               { Console.WriteLine("Snake Eyes!"); }
+
+                if (diceNumber1 == 6 && diceNumber2 == 6)
+                { Console.WriteLine("Box Cars!"); }
+
+                if (diceNumber1 + diceNumber2==7||
+                    diceNumber1 + diceNumber2==2 ||
+                    diceNumber1 + diceNumber2==3)
+                { Console.WriteLine("Craps, You Lose!"); }
+
+                rollCounter++;
+
+                Console.WriteLine("Roll again?(y/n)");
+                rollAgainAnswer = Console.ReadLine();
+            } while ("y" == rollAgainAnswer);
+            Console.WriteLine("Goodbye!");
+
+
         }
     }
 }
+
